caa7beb85c0ef85b083acf2298b6d6aa59012cb9
krm35 <etcc@protonmail.com>
2023-02-13 15:38:46 +0100

    Allow multiple values for hincrbyCommand

--- src/t_hash.c
+++ src/t_hash.c
@@ -629,28 +629,31 @@ void hincrbyCommand(client *c) {
     unsigned char *vstr;
     unsigned int vlen;
 
-    if (getLongLongFromObjectOrReply(c,c->argv[3],&incr,NULL) != C_OK) return;
-    if ((o = hashTypeLookupWriteOrCreate(c,c->argv[1])) == NULL) return;
-    if (hashTypeGetValue(o,c->argv[2]->ptr,&vstr,&vlen,&value) == C_OK) {
-        if (vstr) {
-            if (string2ll((char*)vstr,vlen,&value) == 0) {
-                addReplyError(c,"hash value is not an integer");
-                return;
-            }
-        } /* Else hashTypeGetValue() already stored it into &value */
-    } else {
-        value = 0;
-    }
+	for (int i = 2; i < c->argc; i += 2){
+		if (getLongLongFromObjectOrReply(c,c->argv[i + 1],&incr,NULL) != C_OK) return;
+		if ((o = hashTypeLookupWriteOrCreate(c,c->argv[1])) == NULL) return;
+		if (hashTypeGetValue(o,c->argv[i]->ptr,&vstr,&vlen,&value) == C_OK) {
+			if (vstr) {
+				if (string2ll((char*)vstr,vlen,&value) == 0) {
+					addReplyError(c,"hash value is not an integer");
+					return;
+				}
+			} /* Else hashTypeGetValue() already stored it into &value */
+		} else {
+			value = 0;
+		}
 
-    oldvalue = value;
-    if ((incr < 0 && oldvalue < 0 && incr < (LLONG_MIN-oldvalue)) ||
-        (incr > 0 && oldvalue > 0 && incr > (LLONG_MAX-oldvalue))) {
-        addReplyError(c,"increment or decrement would overflow");
-        return;
-    }
-    value += incr;
-    new = sdsfromlonglong(value);
-    hashTypeSet(o,c->argv[2]->ptr,new,HASH_SET_TAKE_VALUE);
+		oldvalue = value;
+		if ((incr < 0 && oldvalue < 0 && incr < (LLONG_MIN-oldvalue)) ||
+			(incr > 0 && oldvalue > 0 && incr > (LLONG_MAX-oldvalue))) {
+			addReplyError(c,"increment or decrement would overflow");
+			return;
+		}
+		value += incr;
+		new = sdsfromlonglong(value);
+		hashTypeSet(o,c->argv[i]->ptr,new,HASH_SET_TAKE_VALUE);
+	}
+
     addReplyLongLong(c,value);
     signalModifiedKey(c,c->db,c->argv[1]);
     notifyKeyspaceEvent(NOTIFY_HASH,"hincrby",c->argv[1],c->db->id);
